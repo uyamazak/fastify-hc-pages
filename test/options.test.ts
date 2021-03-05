535ef2bf0192edd6c6c894a8b4a811595b4206f2
uyamazak <yu.yamazaki85@gmail.com>
2021-03-05 17:00:02 +0900

    use @tsconfig/node12/tsconfig.json and fix types

--- test/options.test.ts
+++ test/options.test.ts
@@ -2,42 +2,46 @@ import { test } from 'tap'
 import fastify from 'fastify'
 import { hcPages } from '../src/index'
 
-test('set pageNum 5', async (t) => {
+interface Test {
+  tearDown(cb: unknown): unknown
+}
+
+test('set pageNum 5', async (t: Test) => {
   const server = fastify()
   const options = { pagesNum: 5 }
   await server.register(hcPages, options)
   t.tearDown(server.close.bind(server))
 })
 
-test('set pageTimeoutMilliseconds', async (t) => {
+test('set pageTimeoutMilliseconds', async (t: Test) => {
   const server = fastify()
   const options = { pageOptions: { pageTimeoutMilliseconds: 30000 } }
   await server.register(hcPages, options)
   t.tearDown(server.close.bind(server))
 })
 
-test('set userAgent', async (t) => {
+test('set userAgent', async (t: Test) => {
   const server = fastify()
   const options = { pageOptions: { userAgent: 'testUserAgentString' } }
   await server.register(hcPages, options)
   t.tearDown(server.close.bind(server))
 })
 
-test('set emulateMediaTypeScreenEnabled', async (t) => {
+test('set emulateMediaTypeScreenEnabled', async (t: Test) => {
   const server = fastify()
   const options = { pageOptions: { emulateMediaTypeScreenEnabled: true } }
   await server.register(hcPages, options)
   t.tearDown(server.close.bind(server))
 })
 
-test('set acceptLanguage', async (t) => {
+test('set acceptLanguage', async (t: Test) => {
   const server = fastify()
   const options = { pageOptions: { acceptLanguage: 'ja' } }
   await server.register(hcPages, options)
   t.tearDown(server.close.bind(server))
 })
 
-test('set viewport', async (t) => {
+test('set viewport', async (t: Test) => {
   const server = fastify()
   const options = { pageOptions: { viewport: { width: 1920, height: 1080 } } }
   await server.register(hcPages, options)
